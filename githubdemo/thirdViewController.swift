840d5623288a76d2ae8e73b6daed338f1b254ac0
Snehasunkuteja <51108250+Snehasunkuteja@users.noreply.github.com>
2020-02-06 14:41:21 +0530

    Add files via upload

---
+++ githubdemo/thirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  thirdViewController.swift
+//  githubdemo
+//
+//  Created by Touqueir Ahmad on 06/02/20.
+//  Copyright © 2020 Touqueir Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("snehasunku")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
